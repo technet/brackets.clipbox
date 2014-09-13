3535532c9d07ba5037988e22f40459421ae150b0
technet <technetlk@gmail.com>
2014-09-13 17:10:44 +1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-brackets.clipbox
-================
+Brackets ClipBox (Clipboard History)
+=======================
 
-Brackets extension which keeps history of copied texts  and allow you to paste them back
+Another simple extension which remembers last few copied texts and allow you to access them and paste back whenever needed. This will capture <kbd>Ctrl</kbd>+<kbd>C</kbd> keystrokes and store selected text in its own internal array. Then you can invoke the hot-key <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>V</kbd> to launch `QuickOpen` window, select and paste back.
+
+> This extension won't block your usual editor usage, but it seamlessly keeps history of copied texts for future reference.
+
+There are some limitations, but considering 99% of common usage, these limitations won't be significant.
+
+### Usage
+
+* Use <kbd>Ctrl</kbd>+<kbd>C</kbd> as usual to copy texts and continue work on editor. In background `ClipBox` will capture texts and store.
+
+* Hit <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>V</kbd> to invoke `QuickOpen` box to see the history of copied texts, and use <kbd>Up</kbd> and <kbd>Down</kbd> arrow keys to navigate through the entries. Then hit <kbd>Enter</kbd> to paste the selected entry back to the editor.
+
+* Hit <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>E</kbd> to clear the stored history.
+
+* This will store maximum of 10 history entries.
+
+![QuickOpen screenshot](screenshot.png)
+
+### Limitations
+
+* **No Cut, only Copy** In ideal environment this should capture both `Copy` and `Cut` functionality. However I implemented this just by handling the key event and then accessing the editor. During `Copy` operation selected text is available on the editor so we can capture a copy of it. But in the event of `Cut` system has already removed selected text from the editor so unless you directly access clipboard we don't have access to this text. I'm still not sure how to access clipboard directly using Javascript in this environment so left it for future enhancements.
+
+	* I could have used direct key binding using `KeyBindingManager` to capture both <kbd>Ctrl</kbd>+<kbd>C</kbd> and <kbd>Ctrl</kbd>+<kbd>X</kbd> commands, then I would have implemented both scenarios successfully. Problem is that event brackets editor itself avodes [ignoreCommand](https://github.com/adobe/brackets/blob/master/src/editor/EditorCommandHandlers.js#L1106) handling these native commands by itself. Main thing is I just wanted to implement this as a supplementary feature without breaking most primary functionality that any editor has.
+	
+	
+* **Won't handle menu action** This won't respond if you copy using the `Edit -> Copy` menu option. Because with my knowledge I have so far related to brackets, I cannot register another event handler to same command, so I don't know how to know whether user clicks on this menu item.   
+
+### Design Considerations
+
+* Didn't want to introduce new hot key to make this extension usable, which will hinder the normal editor usage, instead you can use usual <kbd>Ctrl</kbd>+<kbd>C</kbd> key combination and extension will capture and build its own clipboard.
+
+* Didn't use `KeyBindingManaer` to capture the keyevent mainly because these are primary key combinations for any editor so didn't want to mess with them.
+
+* Utilized the `QuickOpen` plugin to show the stored clipboard history as it is easy to navigate and select history item.
+
+* Even though it uses `QuickOpen` it doesn't do any filtering mainly because this isn't a search feature. (probably in future releases)
+
+### Future Enhancements
+
+* Capture both `Cut` and `Copy` operations. 
+* Handle `Edit -> Cut` and `Edit -> Copy` menu actions.
+* Filter entries by search string.
---
+++ main.js
@@ -0,0 +1,175 @@
+
+// This is simple extension which remembers copied texts and allow you to access them and paste back.
+// Author: technetlk@gmail.com (https://github.com/technet/brackets.clipbox, http://tutewall.com)
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $, _ */
+define(function (require, exports, module) {
+    "use strict";
+    
+    var AppInit         = brackets.getModule("utils/AppInit"),
+        EditorManager   = brackets.getModule("editor/EditorManager"),
+        KeyEvent        = brackets.getModule("utils/KeyEvent"),
+        CommandManager  = brackets.getModule("command/CommandManager"),
+        Menus           = brackets.getModule("command/Menus"),
+        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils"),
+        PanelManager    = brackets.getModule("view/PanelManager"),
+        QuickOpen       = brackets.getModule("search/QuickOpen"),
+        StringUtils     = brackets.getModule("utils/StringUtils");
+
+    var ctrlDown            = false,
+        filterEnabled       = false,                        // Future feature.
+        clipBoxData         = [],
+        quickOpenHotKey     = "Ctrl-Alt-V",
+        clearClipBoxHotKey  = "Ctrl-Alt-E",
+        quickOpenMatch      = '#',
+        lastKey             = 0;
+    
+    // Constants
+    var MAX_CLIPBOX_SIZE                = 10,                                   // Maximum number of history entries, if you think you need more just increase.
+        EXT_NAME                        = "technet.clipbox",
+        QUICKOPEN_LABEL                 = "ClipBox",
+        CMDID_SHOWCLIPBOX               = EXT_NAME + "-" + "show.history",      // Command Id for showging history of copied texts
+        CMDID_CLEARCLIPBOX              = EXT_NAME + "-" + "clear.history",     // Command Id to clear the history
+    
+        KEY_EVENT_TYPE_DOWN             = "keydown",
+        KEY_EVENT_TYPE_UP               = "keyup",
+
+        MAX_QUICKOPEN_ENTRY_LEN         = 200;                                  // Maximum length of copied text to be displayed in each entry of QuickOpen list.
+
+    
+
+
+    function saveSelection(editor) {
+        var selectedText = editor.getSelectedText(true);
+        if (selectedText !== "") {
+            clipBoxData.unshift(selectedText);
+            if (clipBoxData.length > MAX_CLIPBOX_SIZE) {
+                clipBoxData.pop();
+            }
+        }
+    }
+
+    // We could have used KeyBindingManager for Ctrl+C however bracket source itself ignore this and let syatem to handle. So I didn't
+    // want to mess with that. But the opportunity is there
+    // https://github.com/adobe/brackets/blob/master/src/editor/EditorCommandHandlers.js#L1106
+    var keyEventHandler = function ($event, editor, event) {
+        
+        //console.log(event.type + " " + event.keyCode + " " + event.metaKey);
+        
+        if (event.keyCode === KeyEvent.DOM_VK_CONTROL) {
+            if (event.type === KEY_EVENT_TYPE_DOWN) {
+                lastKey = 1;
+            } else if (event.type === KEY_EVENT_TYPE_UP) {
+                lastKey = -1;
+            }
+        } else if (event.keyCode === KeyEvent.DOM_VK_C && lastKey !== 0 && event.type === KEY_EVENT_TYPE_UP) {
+            // I need to handle Ctrl+X as well however by the time we handle it edit already lost its data, so cannot retain it
+            // in our store. If we know how to access clipboard then this could be done because data is there in the clipboard.
+            lastKey = 0;
+            saveSelection(editor);
+        } else {
+            lastKey = 0;
+        }
+    };
+    
+    function clearClipboard() {
+        clipBoxData = [];
+    }
+
+    
+    var activeEditorChangeHandler = function ($event, focusedEditor, lostEditor) {
+        if (lostEditor) {
+            $(lostEditor).off("keyEvent", keyEventHandler);
+        }
+
+        if (focusedEditor) {
+            $(focusedEditor).on("keyEvent", keyEventHandler);
+        }
+    };
+    
+    function beginClipBoxSearch() {
+        QuickOpen.beginSearch(quickOpenMatch);
+    }
+
+    function buildCommands() {
+
+        var editMenu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+        CommandManager.register("Show ClipBox", CMDID_SHOWCLIPBOX, beginClipBoxSearch);
+        CommandManager.register("Clear ClipBox", CMDID_CLEARCLIPBOX, clearClipboard);
+
+        editMenu.addMenuDivider();
+        
+        editMenu.addMenuItem(CMDID_SHOWCLIPBOX, quickOpenHotKey);
+        editMenu.addMenuItem(CMDID_CLEARCLIPBOX, clearClipBoxHotKey);
+    }
+
+    function searchClipBox(query, matcher) {
+
+        if (filterEnabled && query !== "#") {
+            query = query.substr(1);  // lose the "#" prefix
+            var stringMatch = (matcher && matcher.match) ? matcher.match.bind(matcher) : QuickOpen.stringMatch;
+            var filtered = _.filter(clipBoxData, function (entry) {
+                var matchData =  stringMatch(entry, query);
+                return (matchData ? true : false);
+            });
+            return filtered;
+        } else {
+            return clipBoxData.slice(0);
+        }
+    }
+
+    function matchClipBoxEntry(query) {
+        return query[0] === quickOpenMatch;
+    }
+
+    function clipEntrySelect(selectedItem) {
+        if (selectedItem) {
+            var editor = EditorManager.getCurrentFullEditor();
+            var currentSelection = editor.getSelection();
+            //https://github.com/adobe/brackets/commit/984ed97ddd0a00e3b4d394e55d68a8b7e281326b   https://github.com/adobe/brackets/issues/1688
+            //editor.document.replaceRange(completion, start, end);
+            editor._codeMirror.replaceRange(selectedItem, currentSelection.start, currentSelection.end);
+        }
+    }
+
+    function clipEntryFormatter(item, query) {
+        var trimmed = $.trim(item);
+        var shortText = trimmed.length === 0 ? "[blank]" : (trimmed.length > MAX_QUICKOPEN_ENTRY_LEN ? StringUtils.truncate(trimmed, MAX_QUICKOPEN_ENTRY_LEN) : trimmed);
+        // var formattedText = QuickOpen.highlightMatch(shortText);
+        return StringUtils.format("<li>{0}</li>", _.escape(shortText));
+    }
+
+    function quickClipBoxDone() {
+    }
+
+    function buildQuickOpen() {
+
+        QuickOpen.addQuickOpenPlugin(
+            {
+                name: EXT_NAME,
+                label: QUICKOPEN_LABEL,
+                languageIds: [],
+                fileTypes: [],
+                done: quickClipBoxDone,
+                search : searchClipBox,
+                match : matchClipBoxEntry,
+                itemFocus: function () {},
+                itemSelect: clipEntrySelect,
+                resultsFormatter: clipEntryFormatter,
+                matcherOptions: { segmentedSearch: false }
+            }
+        );
+    }
+
+
+    AppInit.appReady(function () {
+        
+        var currentEditor = EditorManager.getCurrentFullEditor();
+
+        $(currentEditor).on('keyEvent', keyEventHandler);
+        $(EditorManager).on('activeEditorChange', activeEditorChangeHandler);
+        buildCommands();
+        buildQuickOpen();
+    });
+});
